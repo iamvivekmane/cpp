2f10ef3ababeb8ab4619740e67314811fea3b5a3
vivek <vivekmane172@gmail.com>
2024-08-06 21:55:19 +0530

    Day 14

--- Day14/Day_14.3/src/Main.cpp
+++ Day14/Day_14.3/src/Main.cpp
@@ -1,72 +1,70 @@
-#include<cstring>
-#include<iostream>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
 class Array
 {
 private:
-	int size;	//4 bytes
-	int *arr;	//2/4/8 bytes
+	int size; // 4 bytes
+	int *arr; // 2//4//8 bytes
+
 public:
-	//Array *const this = &a1
-	Array( void ) : size( 0 ), arr( NULL )
+	// Array *const this = &a1
+	Array(void) : size(0), arr(NULL)
 	{
-		cout<<"Array( void )"<<endl;
+		cout << "Array(void)" << endl;
 	}
 
-	//Array *const this = &a1
-	Array( int size )
+	Array(int size)
 	{
-		cout<<"Array( int size )"<<endl;
+		cout << "Array(int size)" << endl;
 		this->size = size;
-		this->arr = new int[ size ];
+		this->arr = new int[size];
 	}
 
-	//Array *const this = &a1
-	void acceptRecord( void )
+	// Array *const this = &a1
+	void accept_record(void)
 	{
-		for( int index = 0; index < this->size; ++ index )
+		for (int i = 0; i < size; i++)
 		{
-			cout<<"Enter element	:	";
-			cin>>this->arr[ index ];
+			cout << "Element " << i + 1 << ":   ";
+			cin >> this->arr[i];
 		}
 	}
 
-	//Array *const this = &a1
-	void printRecord( void )
+	// Array *const this = &a1
+	void print_record(void)
 	{
-		for( int index = 0; index < this->size; ++ index )
-			cout<<this->arr[ index ]<<endl;
-	}
-	//Array *const this = &a1
-	~Array( void )	//Destructor
-	{
-		cout<<"~Array( void )"<<endl;
-		this->clear( );
-	}
-private:
-	//Array *const this = &a1
-	void clear( void )
-	{
-		if( this->arr != NULL )
+		cout << "---------------------" << endl;
+		for (int i = 0; i < size; i++)
 		{
-			delete[] this->arr;
-			this->arr = NULL;
+			cout << "Element " << i + 1 << ":   " << this->arr[i] << endl;
+		}
+	}
+	~Array(void) // Destructor
+	{
+		cout << "~Array(void)" << endl;
+		this->clear();
+	}
+
+private:
+	void clear(void)
+	{
+		if (arr != NULL)
+		{
+			delete[] arr;
+			arr = NULL;
 		}
 	}
 };
-int main( void )
+int main()
 {
-	Array a1(3);
-	a1.acceptRecord();
-	a1.printRecord();
-	return 0;
-}
-int main1( void )
-{
-	Array *ptr = new Array( 3 ) ;	//Array::Array( int size )
-	ptr->acceptRecord();
-	ptr->printRecord();
-	delete ptr;	//Array::~Array( );
+	// Array a1;
+	// a1.accept_record(); // a1.print_record(&a1);
+	// a1.print_record();	// a1.print_record(&a1);
+	// // -----------------------------------------------------------------------------------------------
+	Array *ptr = new Array(3); // Array::Array(3);
+	ptr->accept_record();
+	ptr->print_record(); // Array::~Array();
 	return 0;
 }
