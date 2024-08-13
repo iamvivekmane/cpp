805c795c2c33f63762c5c4fe92ed8c0d386a0600
vivek <vivekmane172@gmail.com>
2024-08-13 18:32:03 +0530

    Day 17

--- Day17/Day_17.16/src/Main.cpp
+++ Day17/Day_17.16/src/Main.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
 using namespace std;
-class Outer // Top level class
-{
-private:
-    int num1;
 
-public:
-    class Inner // Inner class
-    {
-    private:
-        int num2;
-
-    public:
-        Inner(void)
-        {
-            this->num2 = 20;
-        }
-        void print(void)
-        {
-            Outer out;
-            cout << "Number 1  : " << out.num1 << endl;
-            cout << "Number 2  : " << this->num2 << endl;
-        }
-    };
-
-public:
-    Outer(void)
-    {
-        this->num1 = 10;
-    }
-};
 int main()
 {
-    Outer::Inner in;
-    in.print();
+    // Local class
+    class Complex
+    {
+    private:
+        int real;
+        int imaginary;
 
+    public:
+        Complex(void) : real(10), imaginary(20)
+        {
+        }
+        void print_record(void)
+        {
+            cout << "Real number      :   " << this->real << endl;
+            cout << "Imaginary number :   " << this->imaginary << endl;
+        }
+    };
+    Complex c1;
+    c1.print_record();
     return 0;
 }
