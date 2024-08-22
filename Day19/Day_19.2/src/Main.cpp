534e28331fb484ad89ba6f3213b9af86c373f994
vivek <vivekmane172@gmail.com>
2024-08-22 08:34:59 +0530

    Day 19

--- Day19/Day_19.2/src/Main.cpp
+++ Day19/Day_19.2/src/Main.cpp
@@ -4,53 +4,51 @@ class Complex
 {
 private:
     int real;
-    int imag;
+    int imaginary;
 
 public:
     Complex(void)
     {
         this->real = 0;
-        this->imag = 0;
+        this->imaginary = 0;
     }
-    Complex(int real, int imag)
+    Complex(int real, int imaginary)
     {
         this->real = real;
-        this->imag = imag;
+        this->imaginary = imaginary;
     }
     Complex(const Complex &other)
     {
-        cout << "Complex( const Complex &other )" << endl;
+        cout << "Complex (const Complex &other)" << endl;
         this->real = other.real;
-        this->imag = other.imag;
+        this->imaginary = other.imaginary;
     }
-    // const Complex &other = c1
-    // Complex *const this = &c2
-    Complex &operator=(const Complex &other)
+    Complex operator=(const Complex &other)
     {
-        cout << "void operator=( const Complex &other )" << endl;
+        cout << "void operator=(const Complex &other)" << endl;
         this->real = other.real;
-        this->imag = other.imag;
+        this->imaginary = other.imaginary;
         return *this;
     }
     friend ostream &operator<<(ostream &cout, const Complex &other)
     {
-        cout << "Real Number	:	" << other.real << endl;
-        cout << "Imag Number	:	" << other.imag << endl;
+        cout << "Real number      :   " << other.real << endl;
+        cout << "Imaginary number :   " << other.imaginary << endl;
         return cout;
     }
 };
-int main(void)
+int main()
 {
-    Complex c1(10, 20), c2, c3;
-    c3 = c2 = c1; // c3.operator =( c2.operator =(c1) )
-    cout << c3 << endl;
-    return 0;
-}
-int main1(void)
-{
+    // Complex c1(10, 20);
+    // Complex c2;
+    // c2 = c1;
+    // cout << c2;
+    // // ----------------------------------------------------------------
+
     Complex c1(10, 20);
     Complex c2;
-    c2 = c1; // c2.operator=( c1 );
-    cout << c2 << endl;
+    Complex c3;
+    c3 = c2 = c1; // c3.operator = (c2.operator = (c1))
+    cout << c3 << endl;
     return 0;
 }
