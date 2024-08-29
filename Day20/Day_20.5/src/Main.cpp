705e3decf4c2d459cd58a4467a16d41fb405a196
vivek <vivekmane172@gmail.com>
2024-08-29 22:18:59 +0530

    Day 20

--- Day20/Day_20.5/src/Main.cpp
+++ Day20/Day_20.5/src/Main.cpp
@@ -7,22 +7,48 @@ private:
     int imaginary;
 
 public:
+    Complex(void)
+    {
+        cout << "Complex(void)" << endl;
+        this->real = 0;
+        this->imaginary = 0;
+    }
     Complex(int value)
     {
+        cout << "Complex(int value)" << endl;
         this->real = value;
         this->imaginary = value;
     }
+    Complex(int real, int imaginary)
+    {
+        cout << "Complex(int real, int imaginary)" << endl;
+        this->real = real;
+        this->imaginary = imaginary;
+    }
+    Complex &operator=(const Complex &other)
+    {
+        cout << "Complex &operator=(const Complex &other)" << endl;
+        this->real = other.real;
+        this->imaginary = other.imaginary;
+        return *this;
+    }
+    // Conversion function
+    operator int()
+    {
+        return this->real;
+    }
     void print_record(void)
     {
-        cout << "Real number      :   " << this->real << endl;
-        cout << "Imaginary number :   " << this->real << endl;
+        cout << "Real       :   " << this->real << endl;
+        cout << "Imaginary  :   " << this->imaginary << endl;
     }
 };
 int main()
 {
-    // int number = 10;
-    // Complex c1 = number;
-    // c1.print_record();
-    // // --------------------------------------------
+    int number = 10;
+    Complex c1 = number;
+    // Complex c1(number);
+    c1.print_record();
+
     return 0;
 }
--- Day20/Day_20.6/src/Main.cpp
+++ Day20/Day_20.6/src/Main.cpp
@@ -1,28 +1,143 @@
 #include <iostream>
+#include <string>
 using namespace std;
-class Complex
+class StackOverflowException
 {
 private:
-    int real;
-    int imaginary;
+    string message;
 
 public:
-    Complex(int value)
+    StackOverflowException(string message = "StackOverflow Exception") : message(message) {}
+    string get_message(void) const throw()
     {
-        this->real = value;
-        this->imaginary = value;
-    }
-    void print_record(void)
-    {
-        cout << "Real number      :   " << this->real << endl;
-        cout << "Imaginary number :   " << this->real << endl;
+        return this->message;
     }
 };
+class StackUnderflowException
+{
+private:
+    string message;
+
+public:
+    StackUnderflowException(string message = "StackUnderflow Exception") : message(message) {}
+    string get_message(void) const throw()
+    {
+        return this->message;
+    }
+};
+template <typename T>
+class Stack
+{
+private:
+    int top;
+    int size;
+    T *arr;
+
+public:
+    Stack(int size) throw(bad_alloc)
+    {
+        this->top = -1;
+        this->size = size;
+        this->arr = NULL;
+        if (this->size > 0)
+        {
+            this->arr = new T[this->size];
+        }
+
+        this->arr = new int[this->size];
+    }
+    bool empty(void) const throw()
+    {
+        return this->top == -1;
+    }
+    bool full(void) const throw()
+    {
+        return this->top == this->size - 1;
+    }
+    void push(T data) throw(StackOverflowException)
+    {
+        if (this->full())
+        {
+            throw StackOverflowException("Stack is full");
+        }
+        this->arr[this->top++] = data;
+    }
+    T peek(void) const throw(StackUnderflowException)
+    {
+        if (this->empty())
+        {
+            throw StackUnderflowException("Stack is empty");
+        }
+
+        return this->arr[this->top];
+    }
+    void pop(void) throw(StackUnderflowException)
+    {
+        if (this->empty())
+        {
+            throw StackUnderflowException("Stack is empty");
+        }
+        this->top--;
+    }
+    ~Stack(void) throw()
+    {
+        if (this->arr != NULL)
+        {
+            delete[] this->arr;
+            arr = NULL;
+        }
+    }
+};
+void accept_record(int &data)
+{
+    cout << "Element        :   ";
+    cin >> data;
+}
+void print_record(const int &data)
+{
+    cout << "Popped element :   " << data << endl;
+}
+int menu_list(void)
+{
+    int choice;
+    cout << "0.Exit" << endl;
+    cout << "1.Push" << endl;
+    cout << "2.Pop" << endl;
+    cout << "Choice         :   ";
+    cin >> choice;
+    return choice;
+}
 int main()
 {
-    // int number = 10;
-    // Complex c1 = number;
-    // c1.print_record();
-    // // --------------------------------------------
+    int choice;
+    int data;
+    Stack<int> s1(5);
+    while ((choice = menu_list()) != 0)
+    {
+        try
+        {
+            switch (choice)
+            {
+            case 1:
+                accept_record(data);
+                s1.push(data);
+                break;
+            case 2:
+                data = s1.peek();
+                print_record(data);
+                s1.pop();
+                break;
+            }
+        }
+        catch (StackOverflowException &ex)
+        {
+            cout << ex.get_message() << endl;
+        }
+        catch (StackUnderflowException &ex)
+        {
+            cout << ex.get_message() << endl;
+        }
+    }
+
     return 0;
 }
